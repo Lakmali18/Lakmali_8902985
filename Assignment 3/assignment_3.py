80a0e37c495654ac8b137cf98a99c087b770b375
LakmaliPalihakkara <lpalihakkara2985@conestogac.on.ca>
2023-07-11 02:19:39 -0400

    file added

---
+++ Assignment 3/assignment_3.py
@@ -0,0 +1,45 @@
+# Importing required libraries
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Setting up the webdriver
+driver = webdriver.Chrome()
+
+# Navigating to the cineplex.com homepage
+driver.get("https://www.cineplex.com/")
+time.sleep(3)
+
+# Clicking on search icon button
+search_icon = driver.find_element("xpath","/html/body/div/div[3]/div/header/nav/div[2]/ul/li[3]/button")
+search_icon.click()
+time.sleep(5)
+
+# Finding the search bar and entering text
+search_bar = driver.find_element("xpath","/html/body/div/div[3]/div/div[5]/div[2]/div/div/div/form/div/div/div/input")
+search_bar.send_keys("transformers")
+# Submitting the search query
+search_bar.send_keys(Keys.RETURN)
+# Waiting for the search results page to load
+time.sleep(5)
+
+# Verifying that the search results page has loaded
+assert "Cineplex.com | Search 2015" in driver.title
+
+# Selecting a transformers English movie from the search results
+movie_link = driver.find_element("xpath","/html/body/div[1]/div[1]/div/div[2]/div/div[4]/div/div/div/div/div[1]/div[2]/div[1]/div/div[1]/div/div[2]/a")
+# Scrolling down
+driver.execute_script("arguments[0].scrollIntoView();", movie_link)
+# Clicking on find show time button
+driver.execute_script("arguments[0].click();", movie_link)
+time.sleep(5)
+
+# Clicking on show time button
+showtime = driver.find_element("xpath","/html/body/div/div[3]/div/div[5]/div[2]/div/div/div/div/div[1]/div[3]/div[1]/div[2]/div[2]/div[3]/div[1]")
+# Scrolling down
+driver.execute_script("arguments[0].scrollIntoView();", showtime)
+showtime.click()
+time.sleep(5)
+
+# Closing the webdriver
+driver.close()
